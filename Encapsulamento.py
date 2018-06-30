8b1ec9747c66a211f9f64b13924dbd4f47942680
= <=>
2018-06-30 01:38:23 -0300

    Enviando

---
+++ Encapsulamento.py
@@ -0,0 +1,8 @@
+#Programa encapsulamento pega a classe LinguagemPOO
+import LinguagemPOO as POO
+
+Pedro = POO.Cachorro("Poodle", "Pedro", 12)
+Estela = POO.Animal("Animal","Borboleta")
+Estela.get_raca()
+Pedro.get_idade()
+Pedro.get_raca()
